8ff8c2cb0ac71978bedbf7fbe6e38735ae0d1ff3
Andrew Van Slaars <avanslaars@gmail.com>
2018-02-02 12:42:42 -0500

    Tested controlled input

--- cypress/integration/form-input.spec.js
+++ cypress/integration/form-input.spec.js
@@ -3,4 +3,13 @@ describe('Form Input', () => {
     cy.visit('/')
     cy.focused().should('have.class', 'new-todo')
   })
+
+  it.only('Accepts input', () => {
+    const typedText = 'New todo'
+    cy.visit('/')
+    cy
+      .get('.new-todo')
+      .type(typedText)
+      .should('have.value', typedText)
+  })
 })
