646329d36de2be97d506d77cfd11ebc9552744e2
Andrew Van Slaars <avanslaars@gmail.com>
2018-02-02 15:47:59 -0500

    Wait for XHR call

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,9 +1,12 @@
 describe('App initialization', () => {
   it('Displays todos from API on load', () => {
     cy.server()
-    cy.route('GET', '/api/todos', 'fixture:todos')
+    cy.route('GET', '/api/todos', 'fixture:todos').as('load')
 
     cy.visit('/')
+
+    cy.wait('@load')
+
     cy.get('.todo-list li').should('have.length', 4)
   })
 })
