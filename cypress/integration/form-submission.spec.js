e3fe900917a2310e148b32f49c8195921fa129c0
Andrew Van Slaars <avanslaars@gmail.com>
2018-02-02 19:59:06 -0500

    Test form submission failure

--- cypress/integration/form-submission.spec.js
+++ cypress/integration/form-submission.spec.js
@@ -21,4 +21,30 @@ describe('Form submission', () => {
 
     cy.get('.todo-list li').should('have.length', 5)
   })
+
+  it('Shows an error message for a failed form submission', () => {
+    const newTodo = 'Test'
+    cy.server()
+    cy
+      .route({
+        method: 'POST',
+        url: '/api/todos',
+        status: 500,
+        response: {}
+      })
+      .as('save')
+
+    cy.seedAndVisit()
+
+    cy
+      .get('.new-todo')
+      .type(newTodo)
+      .type('{enter}')
+
+    cy.wait('@save')
+
+    cy.get('.todo-list li').should('have.length', 4)
+
+    cy.get('.error').should('be.visible')
+  })
 })
