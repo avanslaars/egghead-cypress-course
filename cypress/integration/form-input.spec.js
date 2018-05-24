0fa3ed3566acbbdae9a1c9df84dc59517aa4da10
Zhentian Wan <answer881215@gmail.com>
2018-05-24 16:06:54 +0300

    test focused and placeholder

---
+++ cypress/integration/form-input.spec.js
@@ -0,0 +1,13 @@
+describe('Form input', function () {
+    it('should visit thh app', function () {
+        cy.visit('/');
+    });
+
+    it('should has input filed auto focused when page loaded', function () {
+        cy.visit('/');
+        cy.focused()
+            .should('have.class', 'new-todo')
+            .and('have.attr', 'placeholder', 'What needs to be done?')
+            .and('be.empty');
+    });
+});
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -4,6 +4,7 @@ export default props =>
   <form onSubmit={props.handleTodoSubmit}>
     <input
       type='text'
+      autoFocus
       value={props.currentTodo}
       onChange={props.handleNewTodoChange}
       className="new-todo"
