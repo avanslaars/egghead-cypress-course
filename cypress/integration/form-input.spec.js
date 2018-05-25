a3b70979368ad2eefadb5f1b477614c478a5148e
Zhentian Wan <answer881215@gmail.com>
2018-05-25 16:13:08 +0300

    using fixtures

--- cypress/integration/form-input.spec.js
+++ cypress/integration/form-input.spec.js
@@ -20,13 +20,13 @@ describe('Form input', function () {
 
     it('should have four initial todos', function () {
         cy.server();
-        cy.route('GET', '/api/todos', [
-            {id: 1, name: 'one', isComplete: false},
-            {id: 2, name: 'two', isComplete: false},
-            {id: 3, name: 'three', isComplete: false},
-            {id: 4, name: 'four', isComplete: false}
-        ]);
-
+        /*
+        cy.fixture('todos')
+            .then(todos => {
+                cy.route('GET', '/api/todos', todos);
+            });
+            */
+        cy.route('GET', '/api/todos', 'fixture:todos');
         cy.visit('/');
 
         cy.get('.todo-list > li')
