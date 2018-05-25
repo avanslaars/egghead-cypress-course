760da49fd28ce1f3521aa7e6026ce127f4b7a78a
Zhentian Wan <answer881215@gmail.com>
2018-05-25 16:00:52 +0300

    server the mock data from the testing

--- cypress/integration/form-input.spec.js
+++ cypress/integration/form-input.spec.js
@@ -17,4 +17,19 @@ describe('Form input', function () {
             .type(typedText)
             .should('have.value', typedText);
     });
+
+    it('should have four initial todos', function () {
+        cy.server();
+        cy.route('GET', '/api/todos', [
+            {id: 1, name: 'one', isComplete: false},
+            {id: 2, name: 'two', isComplete: false},
+            {id: 3, name: 'three', isComplete: false},
+            {id: 4, name: 'four', isComplete: false}
+        ]);
+
+        cy.visit('/');
+
+        cy.get('.todo-list > li')
+            .should('have.length', 4);
+    });
 });
