f84a77f269e9ce63bd1de908518b0089dfc350ec
Zhentian Wan <answer881215@gmail.com>
2018-07-03 20:55:19 +0300

    deal with hidden element

--- cypress/integration/form-input.spec.js
+++ cypress/integration/form-input.spec.js
@@ -55,4 +55,73 @@ describe('Form input', function () {
         cy.get('.todo-list > li')
             .should('have.length', 0);
     });
+
+    it('should post new todo to the backend', function () {
+        // Serve the page
+        cy.server();
+
+        // Prepare a POST request
+        cy.route({
+            method: 'POST',
+            url: '/api/todos',
+            response: {id: 123, name: 'new todo', isComplete: false}
+        }).as('save');
+
+        // Call a custom command to load initial todos
+        cy.seedAndVisit();
+
+        // Enter a new todo
+        cy.get('.new-todo')
+            .type('new todo')
+            .type('{enter}');
+
+        // Wait network request to be finished
+        cy.wait('@save');
+
+        // Calculate the expected length of todos
+        cy.get('.todo-list li')
+            .should('have.length', 5);
+    });
+
+    it('should show an error message for a failed from subission', function () {
+        const newTodo = "Test";
+        cy.server();
+        cy.route({
+            method: 'POST',
+            url: '/api/todos',
+            status: 500,
+            response: {}
+        }).as('save');
+
+        cy.seedAndVisit();
+
+        cy.get('.new-todo')
+            .type(newTodo)
+            .type('{enter}');
+
+        cy.wait('@save');
+
+        cy.get('.todo-list li').should('have.length', 4);
+        cy.get('.error').should('be.visible');
+    });
+
+    it('should Delete an item', function () {
+        cy.server();
+        cy.route({
+            method: 'DELETE',
+            url: '/api/todos/*',
+            response: {}
+        }).as('delete');
+
+        cy.seedAndVisit();
+
+        cy.get('.todo-list li')
+            .first()
+            .find('.destroy')
+            .invoke('show') // Make the hidden button appear
+            .click();
+
+        cy.get('.todo-list li')
+            .should('have.length', 3);
+    });
 });
